614e4333729c2d9555c53a64ed4b95b51c68e71f
tylrishabh <32161592+tylrishabh@users.noreply.github.com>
2024-12-29 01:55:58 -0500

    Time: 19 ms (1.99%), Space: 43.6 MB (5.05%) - LeetHub

---
+++ 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int max = -99;
+        List<Boolean> boolList = new ArrayList<>();
+
+        for(int i:candies){
+            max = Math.max(max, i);
+        }
+
+        for(int i:candies){
+            System.out.println((i + extraCandies) +" "+ max);
+            if(i + extraCandies >= max)
+                boolList.add(true);
+            else
+                boolList.add(false);
+        }
+        return boolList;
+    }
+}
